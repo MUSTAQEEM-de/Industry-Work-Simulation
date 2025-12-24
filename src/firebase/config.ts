7919466abb21981bd59890766a367d54d7e6029b
Mustaqeem <mustaqeempachapuri2005@gmail.com>
2025-12-24 05:23:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "studio-9318080426-76f7d",
+  "appId": "1:106181762110:web:b610e690f853413fd9c56f",
+  "apiKey": "AIzaSyB1_AlOiGyC9hRQGevoL7Z444igPl_VI5w",
+  "authDomain": "studio-9318080426-76f7d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "106181762110"
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,24 +1,10 @@
 import { FirebaseApp, getApps, initializeApp } from 'firebase/app';
 import { firebaseConfig } from './config';
-import {
-  Auth,
-  getAuth,
-  connectAuthEmulator,
-  inMemoryPersistence,
-  setPersistence,
-} from 'firebase/auth';
-import {
-  Firestore,
-  getFirestore,
-  connectFirestoreEmulator,
-} from 'firebase/firestore';
 
 export * from './provider';
 export * from './auth/use-user';
 
 let app: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
 
 export const initializeFirebase = (): FirebaseApp | null => {
   if (getApps().length > 0) {
